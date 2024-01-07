250aa7b4d1de4db3cda8d250a10beb38d1c7c3bb
Adnan <adbak.dev@gmail.com>
2024-01-07 21:34:33 +0100

    fc

--- control/bank.go
+++ control/bank.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	var accountBalance = 1000.0
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do ?")
 	fmt.Println("1. Check Balance")
@@ -12,6 +14,25 @@ func main() {
 
 	var choice int
 	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Your balance is ", accountBalance)
+	} else if choice == 2 {
+		fmt.Print("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount
+		fmt.Println("Balance updated! New amount: ", accountBalance)
+	} else if choice == 3 {
+		fmt.Print("Withdrawal amount: ")
+		var withdrawalAmount float64
+		fmt.Scan(&withdrawalAmount)
+		accountBalance -= withdrawalAmount
+		fmt.Println("Balance updated! New amount : ", accountBalance)
+
+	} else {
+		fmt.Println(("Goodbye!"))
+	}
 
 	fmt.Println("Your choice: ", choice)
 }
