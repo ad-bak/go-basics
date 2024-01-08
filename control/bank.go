5b28111042cb1c9d8fb82d0dcbe298b45a934a7e
Adnan <adbak.dev@gmail.com>
2024-01-08 19:34:18 +0100

    fc

--- control/bank.go
+++ control/bank.go
@@ -1,38 +1,15 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"strconv"
+
+	"example.com/bank/fileops"
 )
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-	data, err := os.ReadFile(accountBalanceFile)
-
-	if err != nil {
-		return 1000, errors.New("Failed to find balance file.")
-	}
-
-	balanceText := string(data)
-	balance, err := strconv.ParseFloat(balanceText, 64)
-
-	if err != nil {
-		return 1000, errors.New("Failed to parse stored balance value.")
-	}
-
-	return balance, nil
-}
-
-func writeBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
-}
-
 func main() {
-	var accountBalance, err = getBalanceFromFile()
+	var accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile)
 
 	if err != nil {
 		fmt.Println("ERROR")
@@ -63,7 +40,7 @@ func main() {
 
 			accountBalance += depositAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-			writeBalanceToFile(accountBalance)
+			fileops.WriteBalanceToFile(accountBalance, accountBalanceFile)
 		case 3:
 			fmt.Print("Withdrawal amount: ")
 			var withdrawalAmount float64
@@ -82,7 +59,7 @@ func main() {
 			accountBalance -= withdrawalAmount
 			fmt.Println("Balance updated! New amount : ", accountBalance)
 
-			writeBalanceToFile(accountBalance)
+			fileops.WriteBalanceToFile(accountBalance, accountBalanceFile)
 		default:
 			fmt.Println(("Goodbye!"))
 			fmt.Println("Thanks for choosing our bank")
