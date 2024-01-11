9ad1b3014e80e3285a35ad3bf7007c4c119bbd9e
Adnan <adbak.dev@gmail.com>
2024-01-11 22:28:15 +0100

    Note

---
+++ notes/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/notes/note"
+)
+
+func main() {
+	title, content := getNoteData()
+
+	userNote, err := note.New(title, content)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	userNote.Display()
+
+}
+
+func getNoteData() (string, string) {
+	title := getUserInput("Note title:")
+
+	content := getUserInput("Note content:")
+
+	return title, content
+}
+
+func getUserInput(prompt string) string {
+	fmt.Print(prompt)
+
+	var value string
+	fmt.Scan(&value)
+
+	return value
+}
---
+++ notes/note/note.go
@@ -0,0 +1,29 @@
+package note
+
+import (
+	"errors"
+	"fmt"
+	"time"
+)
+
+type Note struct {
+	title     string
+	content   string
+	createdAt time.Time
+}
+
+func (n Note) Display() {
+	fmt.Printf("Your note title %v has the following content:\n\n%v\n", n.title, n.content)
+}
+
+func New(title, content string) (*Note, error) {
+	if title == "" || content == "" {
+		return &Note{}, errors.New("Invalid input.")
+	}
+
+	return &Note{
+		title:     title,
+		content:   content,
+		createdAt: time.Now(),
+	}, nil
+}
