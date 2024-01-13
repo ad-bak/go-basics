e8a88a7ea9ae534e396aa40bf779ff20bcdae9b0
Adnan <adbak.dev@gmail.com>
2024-01-13 04:22:24 +0100

    todo

--- notes/main.go
+++ notes/main.go
@@ -10,6 +10,27 @@ import (
 	"example.com/notes/todo"
 )
 
+// In Go, interfaces define a set of methods to be implemented by a type.
+// An interface, like 'saver', declares required methods, in this case, Save() returning an error.
+// Types like Note and Todo implement the saver interface by defining Save() with the same signature.
+// This is analogous to JavaScript's abstract classes: saver is the abstract class, Note and Todo are the inheriting classes.
+// Both Note and Todo can be passed to functions like saveData() as they adhere to the saver interface,
+// specifically by implementing the required Save() method.
+// The key is matching the function type and return statement with the interface's method signature.
+
+type saver interface {
+	Save() error
+}
+
+type displayer interface {
+	Display()
+}
+
+type outputtable interface {
+	saver
+	Display()
+}
+
 func main() {
 	title, content := getNoteData()
 
@@ -22,33 +43,39 @@ func main() {
 		return
 	}
 
-	todo.Display()
-	err = todo.Save()
+	userNote, err := note.New(title, content)
 
 	if err != nil {
 		fmt.Println("Saving the todo failed.")
 		return
 	}
 
-	fmt.Println("Saving the todo succeeded!")
-
-	userNote, err := note.New(title, content)
+	err = outputData(todo)
 
 	if err != nil {
-		fmt.Println(err)
 		return
 	}
 
-	userNote.Display()
-	err = userNote.Save()
+	err = outputData(userNote)
+
+}
+
+func saveData(data saver) error {
+	err := data.Save()
 
 	if err != nil {
-		fmt.Println("Saving the note failed.")
-		return
+		fmt.Println("Saving the data failed.")
+		return err
 	}
 
-	fmt.Println("Saving the note succeeded!")
+	fmt.Println("Saving the data succeeded!")
 
+	return nil
+}
+
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
 }
 
 func getNoteData() (string, string) {
