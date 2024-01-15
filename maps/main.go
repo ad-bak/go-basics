97ed0a95c1d1f9f878ba962ff398a24c4c10f406
Adnan <adbak.dev@gmail.com>
2024-01-15 21:54:05 +0100

    funcs

--- maps/main.go
+++ maps/main.go
@@ -2,18 +2,47 @@ package main
 
 import "fmt"
 
+// type alias that will be used in the main function
+// we are saying that floatMap is a map that has string as key and float64 as value
+type floatMap map[string]float64
+
+// here we are adding a method to the floatMap type
+func (m floatMap) output() {
+	fmt.Println(m)
+}
+
 func main() {
+	//here we are allocating memory for 2 strings and we are saying that the capacity of the array is 5
+	// if we overpass the capacity of the array, go will allocate more memory for us
+	// but if we know in advance how many elements we will have in the array, we can allocate memory for them
+	// and this will be more efficient
 	userNames := make([]string, 2, 5)
+
+	userNames[0] = "tonymontana"
 
 	userNames = append(userNames, "johndoe")
 	userNames = append(userNames, "maxpayne")
 
 	fmt.Println(userNames)
 
-	courseRatings := map[string]float64{}
+	// same as above, but we are allocating memory for 3 strings and we are saying that the capacity of the array is 3
+	// when we append the third element, go will allocate more memory for us
+	courseRatings := make(floatMap, 3)
 
 	courseRatings["go"] = 5.0
 	courseRatings["react"] = 4.8
+	courseRatings["python"] = 4.7
+	courseRatings["ruby"] = 4.5
 
-	fmt.Println(courseRatings)
+	courseRatings.output()
+
+	for index, value := range userNames {
+		fmt.Println("index:", index)
+		fmt.Println("value:", value)
+	}
+
+	for key, value := range courseRatings {
+		fmt.Println("key:", key)
+		fmt.Println("value:", value)
+	}
 }
