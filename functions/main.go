ef5da25ee64f279a57c002cea042b0619d3530ce
Adnan <adbak.dev@gmail.com>
2024-01-15 22:15:47 +0100

    closure

--- functions/main.go
+++ functions/main.go
@@ -2,32 +2,45 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3}
+
+	// here we use a closure
+	// we create a function that will multiply a number by 2
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
+
+	// then we pass the function to the transformNumbers function
+	// since we already have a function that will multiply a number by 2
+	// we can just pass it to the transformNumbers function
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
-	fmt.Println(numbers)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	fmt.Println("double", doubled)
+	fmt.Println("triple", tripled)
 
+	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
-	for _, value := range *numbers {
-		dNumbers = append(dNumbers, transform(value))
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
 	}
 
 	return dNumbers
 }
 
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
+// this is a closure
+// it's a function that returns another function
+// in this example this function will return a function that will multiply a number by the factor
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
